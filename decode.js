df8df4a31aed9a169febeef1c398819d3d85a5af
Jannis R <mail@jannisr.de>
2021-08-05 13:49:15 +0200

    support --help/-h & --version/-v; 1.0.1
    raised in #4

--- decode.js
+++ decode.js
@@ -1,6 +1,32 @@
 #!/usr/bin/env node
 'use strict'
 
+const mri = require('mri')
+const pkg = require('./package.json')
+
+const argv = mri(process.argv.slice(2), {
+	boolean: [
+		'help', 'h',
+		'version', 'v',
+	]
+})
+
+if (argv.help || argv.h) {
+	process.stdout.write(`
+Usage:
+    echo -n <url-encoded-string> | url-decode
+Examples:
+    echo -n '%7B%22foo%22%3A%20%22bar%22%7D' | url-decode
+    # {"foo": "bar"}
+\n`)
+	process.exit(0)
+}
+
+if (argv.version || argv.v) {
+	process.stdout.write(`${pkg.name} v${pkg.version}\n`)
+	process.exit(0)
+}
+
 const qs = require('querystring')
 
 process.stdin
--- encode.js
+++ encode.js
@@ -1,6 +1,32 @@
 #!/usr/bin/env node
 'use strict'
 
+const mri = require('mri')
+const pkg = require('./package.json')
+
+const argv = mri(process.argv.slice(2), {
+	boolean: [
+		'help', 'h',
+		'version', 'v',
+	]
+})
+
+if (argv.help || argv.h) {
+	process.stdout.write(`
+Usage:
+    echo -n <url> | url-encode
+Examples:
+    echo -n '{"foo": "bar"}' | url-encode
+    # %7B%22foo%22%3A%20%22bar%22%7D
+\n`)
+	process.exit(0)
+}
+
+if (argv.version || argv.v) {
+	process.stdout.write(`${pkg.name} v${pkg.version}\n`)
+	process.exit(0)
+}
+
 const qs = require('querystring')
 
 process.stdin
